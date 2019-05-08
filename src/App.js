4d96ef127e8ab4d698a7136cb35b323e116651ff
Nicolas De Paul <nicolas@Nicolass-MacBook-Pro.local>
2019-05-08 10:50:43 +1000

    Added Bootstrap to the project. Setted up react router. Created 3 components (TodosList, EditTodo, CreateTodo)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() extends Component {
   render() {
     return  (
+      <Router>
       <div className="container">
         <h2>MERN-Stack Todo App</h2>
         </div>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+        </Router>
     );
   }
 }
