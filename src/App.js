65e191245fc3f926bde13279a9a23ef9dda35cc0
Nicolas De Paul <nicolas@Nicolass-MacBook-Pro.local>
2019-05-08 11:08:22 +1000

    Created The Basic Layout & Navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 
-function App() extends Component {
+import logo from "./Amplitude.png";
+
+class App extends Component {
   render() {
     return  (
       <Router>
-      <div className="container">
-        <h2>MERN-Stack Todo App</h2>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://amplitude.com/" target="_blank">
+              <img src={logo} width="138.53" height="70" alt="Amplitude"/>
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                  <li className="navbar-item">
+                    <Link to="/" className="nav-link">Todos</Link>
+                  </li>
+                  <li className="navbar-item">
+                    <Link to="/create" className="nav-link">Create Todo</Link>
+                  </li>
+                </ul>
+              </div>
+              </nav>
+           <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
-        </Router>
+      </Router>
     );
   }
 }
+
 
 export default App;
