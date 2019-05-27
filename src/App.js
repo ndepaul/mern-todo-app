b0d0a012eaa342f6a657b03cb979983198581e37
Nicolas De Paul <nicolas@Nicolass-MacBook-Pro.local>
2019-05-27 11:28:04 +1000

    Small name changes

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ class App extends Component {
             <div className="collpase navbar-collapse">
                 <ul className="navbar-nav mr-auto">
                   <li className="navbar-item">
-                    <Link to="/" className="nav-link">Todos</Link>
+                    <Link to="/" className="nav-link">ToDos</Link>
                   </li>
                   <li className="navbar-item">
-                    <Link to="/create" className="nav-link">Create Todo</Link>
+                    <Link to="/create" className="nav-link">Create To Do</Link>
                   </li>
                 </ul>
               </div>
